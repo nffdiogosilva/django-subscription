153c6f2747a27d5ca5fecdadbb7eff4030abf692
Nuno Diogo da Silva <diogosilva.nuno@gmail.com>
2019-08-12 17:20:35 +0000

    Add unit tests (not passing)
    Add unit tests respecting the "Red Green Refactor" pattern design.
    The written tests respect the project requirements given:
    "A customer should be able to subscribe to plan, move from a plan to another and manage websites (add/update/remove) according to his plan.
    Subscriptions have a 1-year time value."

--- src/confs/settings/tests.py
+++ src/confs/settings/tests.py
@@ -1,1 +1,10 @@
 from .base import *
+
+SECRET_KEY='lHnABhdr8+:WUDV{P5g|dA%dl[K3w#,2/7EpC3br2o`5uHNZfa'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'memory:'
+    }
+}
--- src/subscription/tests.py
+++ src/subscription/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
 
-# Create your tests here.
+# from mixer.backend.django import mixer
+
+from .models import Customer, Plan, Website
+
+
+class CustomerTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_customer_can_buy_plan(self):
+        """Test a customer is eligible to buy a plan"""
+        raise NotImplementedError
+
+    def test_customer_can_change_plans(self):
+        """Test a customer is eligible to change between plans"""
+        raise NotImplementedError
+
+    def test_customer_can_add_website(self):
+        """Test a customer is eligible to add a website"""
+        raise NotImplementedError
+
+    def test_customer_can_remove_website(self):
+        """Test a customer is eligible to remove a website"""
+        raise NotImplementedError
+
+    def test_customer_can_update_website(self):
+        """Test a customer is eligible to update a website"""
+        raise NotImplementedError
+
+    def test_customer_renewal_date_has_one_year_time_value(self):
+        """Test that the subscription renewal date has a one year timestamp"""
+        raise NotImplementedError
+
+
+class PlanTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_plan_type_single_allow_only_one_website(self):
+        """Test that the plan with type single can only have one website"""
+        raise NotImplementedError
+
+    def test_plan_type_plus_allow_3_websites(self):
+        """Test that the plan with type plus can only have max 3 websites"""
+        raise NotImplementedError
+
+    def test_plan_type_infinite_allow_unlimited_websites(self):
+        """Test that the plan with type infinite can have multiple websites"""
+        raise NotImplementedError
