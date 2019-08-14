343e24151bbf663eeef38961a0139cacae2f2eaf
Nuno Diogo da Silva <diogosilva.nuno@gmail.com>
2019-08-14 14:50:32 +0000

    Add base admin area

--- src/subscription/admin.py
+++ src/subscription/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Customer, Plan, Website
+
+admin.site.register(Customer)
+admin.site.register(Plan)
+admin.site.register(Website)
